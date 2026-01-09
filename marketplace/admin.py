bfcf231f07eca16ecd6be006a9a033514335981b
cstuart756 <stujjc@yahoo.co.uk>
2026-01-09 11:22:11 +0000

    Add Product model and admin configuration

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("title", "owner", "price", "created_at")
+    search_fields = ("title", "description")
--- marketplace/models.py
+++ marketplace/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Product(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="products"
+    )
     title = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
