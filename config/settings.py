56cbbf94a969128cbe72bacd88072ebcc6d704e1
cstuart756 <stujjc@yahoo.co.uk>
2026-01-09 11:41:23 +0000

    Add user registration, login, logout, and login state UI

--- config/settings.py
+++ config/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "product_list"
+LOGOUT_REDIRECT_URL = "product_list"
+LOGIN_URL = "login"
+
